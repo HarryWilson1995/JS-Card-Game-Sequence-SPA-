fcbc64249c7c40609d66a1b7541d09652dc08d03
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-07-02 08:19:54 -0400

    added in next steps for JS file initial fetch request

--- frontend/sequence.js
+++ frontend/sequence.js
@@ -2,3 +2,8 @@ const startGameForm = document.querySelector('.startGameForm');
 
 startGameForm.style.display = 'none';
 startGameForm.style.display = 'flex';
+
+// Next step is do a fetch to game create, with data that comes back, create all pages for players
+
+fetch();
+// .then(add in function that creates many html divs for each player, their hand etc)

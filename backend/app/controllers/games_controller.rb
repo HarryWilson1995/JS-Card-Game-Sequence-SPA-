79ac024b051c9558bd6b58b71445092861f111d3
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-07-03 14:44:56 -0400

    setting up javascript to grab hold of form values to send to controller game create value

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -1,8 +1,10 @@
+require 'pry'
 class GamesController < ApplicationController
 
   def create 
+    binding.pry
     game = Game.new
-    game.winner = "Harry"
+    game.winner = nil
     game.player_count = 1
     game.save
     render json: game 
--- frontend/sequence.js
+++ frontend/sequence.js
@@ -1,21 +1,39 @@
 const BASE_URL = 'http://localhost:3000';
+const GAMES_URL = `${BASE_URL}/games`;
 const startGameForm = document.querySelector('.startGameForm');
+const newGameForm = document.getElementById('newGameForm');
 
 startGameForm.style.display = 'none';
 startGameForm.style.display = 'flex';
 
 // Next step is do a fetch to game create, with data that comes back, create all pages for players
 
-startGameForm.addEventListener('submit', (e) => {
+newGameForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  fetch('http://localhost:3000/games', {
+  const playerOne = document.getElementById('player1NameFormInputField').value;
+  const playerTwo = document.getElementById('player2NameFormInputField').value;
+  const playerThree = document.getElementById('player3NameFormInputField')
+    .value;
+  const playerFour = document.getElementById('player4NameFormInputField').value;
+  const playerFive = document.getElementById('player5NameFormInputField').value;
+  const playerSix = document.getElementById('player6NameFormInputField').value;
+  const playerSeven = document.getElementById('player7NameFormInputField')
+    .value;
+
+  fetch(GAMES_URL, {
     method: 'POST',
     credentials: 'same-origin',
     headers: {
       'Content-type': 'application/json',
     },
     body: JSON.stringify({
-      name: 'Harry',
+      player_1: playerOne,
+      player_2: playerTwo,
+      player_3: playerThree,
+      player_4: playerFour,
+      player_5: playerFive,
+      player_6: playerSix,
+      player_7: playerSeven,
     }),
   })
     .then((res) => console.log(res.json()))

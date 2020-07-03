1b52a59852b3da2c1c9031e3ea3dbd38d6fb72c5
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-07-03 17:02:28 -0400

    added logic that creates player for each non empty string coming in from form

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -6,25 +6,175 @@ class GamesController < ApplicationController
     game.player_count = 0
     game.save
     if params["player_1"] != "" 
-      player1 = Player.new 
-      player1.name = params["player_1"]
-      player1.save
+      player = Player.new 
+      player.name = params["player_1"]
+      player.save
       round1 = Round.new 
       round1.round_num = 1
       round1.score = 0
-      round1.player_id = player1.id
+      round1.player_id = player.id
       round1.game_id = game.id
       round1.save
       round2 = Round.new 
       round2.round_num = 2
       round2.score = 0
-      round2.player_id = player1.id
+      round2.player_id = player.id
       round2.game_id = game.id
       round2.save
       round3 = Round.new 
       round3.round_num = 3
       round3.score = 0
-      round3.player_id = player1.id
+      round3.player_id = player.id
+      round3.game_id = game.id
+      round3.save
+      game.player_count +=1
+      game.save
+    end
+    if params["player_2"] != "" 
+      player = Player.new 
+      player.name = params["player_2"]
+      player.save
+      round1 = Round.new 
+      round1.round_num = 1
+      round1.score = 0
+      round1.player_id = player.id
+      round1.game_id = game.id
+      round1.save
+      round2 = Round.new 
+      round2.round_num = 2
+      round2.score = 0
+      round2.player_id = player.id
+      round2.game_id = game.id
+      round2.save
+      round3 = Round.new 
+      round3.round_num = 3
+      round3.score = 0
+      round3.player_id = player.id
+      round3.game_id = game.id
+      round3.save
+      game.player_count +=1
+      game.save
+    end
+    if params["player_3"] != "" 
+      player = Player.new 
+      player.name = params["player_3"]
+      player.save
+      round1 = Round.new 
+      round1.round_num = 1
+      round1.score = 0
+      round1.player_id = player.id
+      round1.game_id = game.id
+      round1.save
+      round2 = Round.new 
+      round2.round_num = 2
+      round2.score = 0
+      round2.player_id = player.id
+      round2.game_id = game.id
+      round2.save
+      round3 = Round.new 
+      round3.round_num = 3
+      round3.score = 0
+      round3.player_id = player.id
+      round3.game_id = game.id
+      round3.save
+      game.player_count +=1
+      game.save
+    end
+    if params["player_4"] != "" 
+      player = Player.new 
+      player.name = params["player_4"]
+      player.save
+      round1 = Round.new 
+      round1.round_num = 1
+      round1.score = 0
+      round1.player_id = player.id
+      round1.game_id = game.id
+      round1.save
+      round2 = Round.new 
+      round2.round_num = 2
+      round2.score = 0
+      round2.player_id = player.id
+      round2.game_id = game.id
+      round2.save
+      round3 = Round.new 
+      round3.round_num = 3
+      round3.score = 0
+      round3.player_id = player.id
+      round3.game_id = game.id
+      round3.save
+      game.player_count +=1
+      game.save
+    end
+    if params["player_5"] != "" 
+      player = Player.new 
+      player.name = params["player_5"]
+      player.save
+      round1 = Round.new 
+      round1.round_num = 1
+      round1.score = 0
+      round1.player_id = player.id
+      round1.game_id = game.id
+      round1.save
+      round2 = Round.new 
+      round2.round_num = 2
+      round2.score = 0
+      round2.player_id = player.id
+      round2.game_id = game.id
+      round2.save
+      round3 = Round.new 
+      round3.round_num = 3
+      round3.score = 0
+      round3.player_id = player.id
+      round3.game_id = game.id
+      round3.save
+      game.player_count +=1
+      game.save
+    end
+    if params["player_6"] != "" 
+      player = Player.new 
+      player.name = params["player_6"]
+      player.save
+      round1 = Round.new 
+      round1.round_num = 1
+      round1.score = 0
+      round1.player_id = player.id
+      round1.game_id = game.id
+      round1.save
+      round2 = Round.new 
+      round2.round_num = 2
+      round2.score = 0
+      round2.player_id = player.id
+      round2.game_id = game.id
+      round2.save
+      round3 = Round.new 
+      round3.round_num = 3
+      round3.score = 0
+      round3.player_id = player.id
+      round3.game_id = game.id
+      round3.save
+      game.player_count +=1
+      game.save
+    end
+    if params["player_7"] != "" 
+      player = Player.new 
+      player.name = params["player_7"]
+      player.save
+      round1 = Round.new 
+      round1.round_num = 1
+      round1.score = 0
+      round1.player_id = player.id
+      round1.game_id = game.id
+      round1.save
+      round2 = Round.new 
+      round2.round_num = 2
+      round2.score = 0
+      round2.player_id = player.id
+      round2.game_id = game.id
+      round2.save
+      round3 = Round.new 
+      round3.round_num = 3
+      round3.score = 0
+      round3.player_id = player.id
       round3.game_id = game.id
       round3.save
       game.player_count +=1

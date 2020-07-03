c1d7ae06960f9fffb5283df308d59483481b9e3b
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-07-03 16:29:14 -0400

    adding relationships to the model files

--- backend/app/models/game.rb
+++ backend/app/models/game.rb
@@ -1,2 +1,4 @@
 class Game < ApplicationRecord
+  has_many :rounds 
+  has_many :players, through: :rounds
 end
--- backend/app/models/round.rb
+++ backend/app/models/round.rb
@@ -1,2 +1,4 @@
 class Round < ApplicationRecord
+  belongs_to :player
+  belongs_to :game
 end

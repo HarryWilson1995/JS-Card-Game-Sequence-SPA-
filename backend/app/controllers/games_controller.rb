e8e5ab5b834e1c8390feaf359d57e7d8643376b3
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-07-06 17:59:12 -0400

    adding to game controller create method to create cards, deck, discard pile and setup associations

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -180,7 +180,26 @@ class GamesController < ApplicationController
       game.player_count +=1
       game.save
     end
+    deck = Deck.new
+    deck.game = game
+    deck.save
+    card_values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", 'J', 'Q', 'K', 'A']
+    card_suits = ["H", "D", "C", "S"]
+    card_suits.each do |current_suit|
+      card_values.each do |current_value|
+        card = Card.new
+        card.suit = current_suit
+        card.value = current_value
+        card.game = game
+        card.locationable = deck
+        card.save 
+      end
+    end
+    discard_pile = DiscardPile.new 
+    discard_pile.game = game 
+    discard_pile.save
     game.winner = nil
-    render json: game, include: [:players, :rounds]
+    game.save
+    render json: game, include: [:players, :rounds, :deck, :cards]
   end
 end

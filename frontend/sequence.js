5d2870739448b12c324f433e7f14ef412014a194
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-07-10 12:04:28 -0400

    added click event on deck so that second card is drawn

--- frontend/sequence.js
+++ frontend/sequence.js
@@ -9,6 +9,7 @@ const cardReveal = document.querySelector('.cardReveal');
 const deckArea = document.querySelector('.deckArea');
 const sequenceDropArea = document.querySelector('.sequenceDropArea');
 const dropZone = document.querySelector('.dropZone');
+const deck = document.querySelector('.deck');
 
 // Drop zone drag over event
 dropZone.addEventListener('dragover', (e) => {
@@ -16,6 +17,31 @@ dropZone.addEventListener('dragover', (e) => {
   const draggable = document.querySelector('.dragging');
   draggable.classList.remove('handCard');
   dropZone.appendChild(draggable);
+});
+
+// Draw Card event
+deck.addEventListener('click', () => {
+  const drawnCard = deck.children[1];
+  const playerScreens = document.querySelectorAll('.playerScreen');
+  playerScreens.forEach((screen) => {
+    if (screen.style.display !== 'none') {
+      drawnCard.classList.add('handCard');
+      drawnCard.classList.remove('deckCard');
+      drawnCard.onmouseenter = function (e) {
+        e.target.style.marginBottom = '5rem';
+      };
+      drawnCard.onmouseleave = function (e) {
+        e.target.style.marginBottom = '0';
+      };
+      drawnCard.addEventListener('dragstart', () => {
+        drawnCard.classList.add('dragging');
+      });
+      drawnCard.addEventListener('dragend', () => {
+        drawnCard.classList.remove('dragging');
+      });
+      screen.children[0].appendChild(drawnCard);
+    }
+  });
 });
 
 newGameForm.addEventListener('submit', (e) => {
@@ -311,7 +337,6 @@ function renderDeck(obj) {
 }
 
 function renderGameInfo(obj) {
-  console.log(obj);
   const scoresList = document.querySelector('.scores');
   const roundInfo = document.querySelector('.roundInfo');
   roundInfo.innerText = 'Round: 1';

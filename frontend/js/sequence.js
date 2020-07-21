35a2dedb8185f864c5eb51d1d8e9dd697b4ec952
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-07-21 19:22:16 -0400

    added validations for same number sets

--- frontend/js/sequence.js
+++ frontend/js/sequence.js
@@ -1,16 +1,60 @@
 class Sequence {
   static consecutiveNumbers(set) {
-    set.forEach((c) => {
-      const item = document.getElementById(c.id);
-      console.log(item);
-    });
-    return true;
+    const card1 = document.getElementById(set[0].id);
+    const card2 = document.getElementById(set[1].id);
+    const card3 = document.getElementById(set[2].id);
+    console.log(card1);
+    console.log(card2);
+    console.log(card3);
+    return false;
   }
   static sameNumbers(set) {
-    set.forEach((c) => {
-      const item = document.getElementById(c.id);
-      console.log(item);
-    });
-    return true;
+    const card1 = document.getElementById(set[0].id);
+    const card2 = document.getElementById(set[1].id);
+    const card3 = document.getElementById(set[2].id);
+    if (
+      card1.dataset.number === card2.dataset.number &&
+      card2.dataset.number === card3.dataset.number
+    ) {
+      return true;
+    } else if (
+      card1.dataset.number === 'joker' &&
+      card2.dataset.number === 'joker' &&
+      card3.dataset.number === 'joker'
+    ) {
+      return true;
+    } else if (
+      card1.dataset.number === card2.dataset.number &&
+      card3.dataset.number === 'joker'
+    ) {
+      return true;
+    } else if (
+      card1.dataset.number === card3.dataset.number &&
+      card2.dataset.number === 'joker'
+    ) {
+      return true;
+    } else if (
+      card2.dataset.number === card3.dataset.number &&
+      card1.dataset.number === 'joker'
+    ) {
+      return true;
+    } else if (
+      card1.dataset.number === 'joker' &&
+      card2.dataset.number === 'joker'
+    ) {
+      return true;
+    } else if (
+      card1.dataset.number === 'joker' &&
+      card3.dataset.number === 'joker'
+    ) {
+      return true;
+    } else if (
+      card2.dataset.number === 'joker' &&
+      card3.dataset.number === 'joker'
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
